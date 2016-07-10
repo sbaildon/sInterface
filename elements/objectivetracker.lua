e01da75b0a844003a98af6fd0ec05cc1d5b13229
Sean Baildon <sean@baildon.co>
2016-07-10 13:34:13 +0100

    Better objective tracker

--- elements/objectivetracker.lua
+++ elements/objectivetracker.lua
@@ -1,17 +1,39 @@
-local otf = ObjectiveTrackerFrame
-otf:SetMovable(true)
-otf:SetUserPlaced(true)
-otf:ClearAllPoints()
-otf:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 40, -25)
-otf:SetMovable(false)
-otf:RegisterEvent("GROUP_ROSTER_UPDATE");
+local unpack = unpack
+local ObjectiveTrackerFrame = ObjectiveTrackerFrame
 
-local function objective_mover(self, event, ...)
-	local raidindex = UnitInRaid("player")
-	if (raidindex == nil) then
-		self:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 40, -25)
-	else
-		self:SetPoint("TOPLEFT", oUF_SkaarjRaid, "BOTTOMLEFT", 30, -25)
-	end
+local frame = CreateFrame("Frame")
+
+local cfg = {}
+cfg.pos = {"TOPRIGHT", UIParent, "TOPLEFT", 280, -20}
+cfg.pos_raid = {"TOPRIGHT", "oUF_SkaarjRaid", "BOTTOMLEFT", 260, -20}
+
+local gettingSet
+local function AdjustSetPoint(self,...)
+	if gettingSet then return end
+ 	gettingSet = true
+ 	local raidindex = UnitInRaid("player")
+ 	local pos = (raidindex == nil) and cfg.pos or cfg.pos_raid
+
+	self:SetPoint(unpack(pos))
+
+    -- if InCombatLockdown() then
+    --   frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+    -- end
+  gettingSet = false
 end
-otf:HookScript("OnEvent", objective_mover);
+
+frame:SetScript("OnEvent", function(self,event)
+  if not event == "GROUP_ROSTER_UPDATE" then
+    self:UnregisterEvent(event)
+  end
+  if event == "PLAYER_LOGIN" then
+    self.point = {ObjectiveTrackerFrame:GetPoint()}
+    hooksecurefunc(ObjectiveTrackerFrame, "SetPoint", AdjustSetPoint)
+  end
+  if not InCombatLockdown() then
+    ObjectiveTrackerFrame:SetPoint(unpack(self.point))
+  end
+end)
+
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:RegisterEvent("GROUP_ROSTER_UPDATE")

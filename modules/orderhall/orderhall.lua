658256f35a76b4441823355926dd148ebf5b3c35
Sean Baildon <sean@baildon.co>
2016-09-03 10:38:32 +0100

    prevent errors hiding orderhall bar

--- modules/orderhall/orderhall.lua
+++ modules/orderhall/orderhall.lua
@@ -1,8 +1,12 @@
 local orderhall = CreateFrame('Frame', 'OrderHall')
 
+orderhall:RegisterEvent('ZONE_CHANGED_NEW_AREA')
 orderhall:RegisterEvent('PLAYER_ENTERING_WORLD')
 orderhall:SetScript('OnEvent', function(self, event, ...)
 	local b = OrderHallCommandBar
+
+	if not b then return end
+
 	b:UnregisterAllEvents()
 	b:SetScript("OnShow", b.Hide)
 	b:Hide()

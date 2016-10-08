ff86203c80a3e06e23209b3d313b34124035996b
Sean Baildon <sean@baildon.co>
2016-10-08 14:56:47 +0100

    talkingheads

---
+++ config/talkinghead.lua
@@ -0,0 +1,7 @@
+local _, ns = ...
+local E, C = ns.E, ns.C
+
+C["talkinghead"] = {
+	position = { "BOTTOM", UIParent, "BOTTOM", 0, E:HeightPercentage(15) },
+	scale = 0.85
+}
---
+++ modules/talkinghead/talkinghead.lua
@@ -0,0 +1,19 @@
+local addon, ns = ...
+local E, C = ns.E, ns.C
+
+local TalkingHeadAnchor = CreateFrame("Frame", addon.."TalkingHeadAnchor", UIParent)
+TalkingHeadAnchor:SetScale(C["talkinghead"].scale)
+TalkingHeadAnchor:SetPoint(unpack(C["talkinghead"].position))
+TalkingHeadAnchor:SetSize(1,1)
+
+TalkingHeadAnchor:RegisterEvent('ADDON_LOADED')
+TalkingHeadAnchor:SetScript('OnEvent', function(self, event, ...)
+	local addonname = ...
+	if (addonname == "Blizzard_TalkingHeadUI") then 
+		TalkingHeadFrame:SetParent(TalkingHeadAnchor)
+		TalkingHeadFrame:ClearAllPoints()
+		TalkingHeadFrame:SetPoint("CENTER")
+		TalkingHeadFrame.ignoreFramePositionManager = true
+		self:UnregisterAllEvents()
+	end
+end)

ec1f770403c505583f1f9905e016984c63a9af9b
Sean Baildon <sean@baildon.co>
2017-09-10 13:01:30 +0100

    config: pretend bag button is micro menu ;-)

--- config/actionbars.lua
+++ config/actionbars.lua
@@ -4,39 +4,39 @@ local C = ns.C
 C["actionbars"] = {
 	enabled = true,
 
-	bar1 = { -- sInterfaceBar1
-		position = { "TOPLEFT", UIParent, "TOPLEFT", C.general.edgeSpacing, -C.general.edgeSpacing},
+	bar1 = { -- sInterfaceActionBar1
+		position = { "TOPLEFT", UIParent, "TOPLEFT", C.general.edgeSpacing, -(C.general.edgeSpacing*15)},
 		visibility = "[modifier:ctrl, modifier:alt, modifier:shift][cursor] show; hide",
 		two_rows = true
 	},
 
-	bar2 = { --sInterfaceBar2
-		position = { "TOPLEFT", "sInterfaceBar1", "BOTTOMLEFT", 0, -20},
+	bar2 = { --sInterfaceActionBar2
+		position = { "TOPLEFT", "sInterfaceActionBar1", "BOTTOMLEFT", 0, -20},
 		visibility = "[modifier:ctrl, modifier:alt, modifier:shift][cursor] show; hide",
 		two_rows = true
 
 	},
 
-	bar3 = { --sInterfaceBar3
-		position = { "TOPLEFT", "sInterfaceBar2", "BOTTOMLEFT", 0, -20},
+	bar3 = { --sInterfaceActionBar3
+		position = { "TOPLEFT", "sInterfaceActionBar2", "BOTTOMLEFT", 0, -20},
 		visibility = "[modifier:ctrl, modifier:alt, modifier:shift][cursor] show; hide",
 		two_rows = true
 	},
 
-	bar4 = { --sInterfaceBar4
-		position = { "TOPLEFT", "sInterfaceBar3", "BOTTOMLEFT", 0, -20},
+	bar4 = { --sInterfaceActionBar4
+		position = { "TOPLEFT", "sInterfaceActionBar3", "BOTTOMLEFT", 0, -20},
 		visibility = "[modifier:ctrl, modifier:alt, modifier:shift][cursor] show; hide",
 		two_rows = true
 	},
 
-	bar5 = { --sInterfaceBar5
-		position = { "TOPLEFT", "sInterfaceBar4", "BOTTOMLEFT", 0, -20},
+	bar5 = { --sInterfaceActionBar5
+		position = { "TOPLEFT", "sInterfaceActionBar4", "BOTTOMLEFT", 0, -20},
 		visibility = "[modifier:ctrl, modifier:alt, modifier:shift][cursor] show; hide",
 		two_rows = true
 	},
 
 	petbar = { --sInterfacePetBar
-		position = { "TOPLEFT", "sInterfaceBar5", "BOTTOMLEFT", 0, -20},
+		position = { "TOPLEFT", "sInterfaceActionBar5", "BOTTOMLEFT", 0, -20},
 		visibility = "[petbattle][overridebar][vehicleui][possessbar,@vehicle,exists] hide; [modifier:ctrl,modifier:alt,modifier:shift,@pet,exists][cursor,@pet,exists] show; hide",
 	},
 
@@ -61,13 +61,13 @@ C["actionbars"] = {
 	},
 
 	bags = { --sInterfaceBagsBar
-		position = { "TOP", UIParent, "TOP", 0, 0},
+		position = { "BOTTOMLEFT", "sInterfaceMicromenu", "BOTTOMRIGHT", -25, 0},
 		visibility = "[modifier:ctrl, modifier:alt, modifier:shift] show; hide",
 		show_all = false
 	},
 
 	micromenu = { --sInterfaceMicromenu
-		position = { "TOP", "sInterfaceBagsBar", "BOTTOM", 0, 0},
+		position = { "TOP", UIParent, "TOP", 0, -C.general.edgeSpacing},
 		visibility = "[modifier:ctrl, modifier:alt, modifier:shift] show; hide",
 		two_rows = true
 	},

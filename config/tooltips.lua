bad557bfbc35d5ebbc756b26436a959f040e3dd0
Sean Baildon <sean@baildon.co>
2017-09-10 09:50:51 +0100

    tooltips: percentage based placement

--- config/tooltips.lua
+++ config/tooltips.lua
@@ -1,10 +1,10 @@
 local _, ns = ...
-local C = ns.C
-  
+local E, C = ns.E, ns.C
+
 C["tooltips"] = {
-	pos  = { "BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -180, 180 },
+	pos  = { "BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -E:WidthPercentage(7), E:HeightPercentage(8) },
 	font = STANDARD_TEXT_FONT,
-	backdrop = { bgFile = "Interface\\Buttons\\WHITE8x8", 
+	backdrop = { bgFile = "Interface\\Buttons\\WHITE8x8",
 		edgeFile = "Interface\\Buttons\\WHITE8x8",
 		tiled = false, edgeSize = 1,
 		insets = { left = 0, right = 0, top = 0, bottom = 0 },

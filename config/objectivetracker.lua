f0141bc35816976b12dd8da2dbfb129947afb745
Sean Baildon <sean@baildon.co>
2017-09-18 16:28:21 +0100

    config: move objective tracker

--- config/objectivetracker.lua
+++ config/objectivetracker.lua
@@ -4,6 +4,6 @@ local C = ns.C
 
 C["objectivetracker"] = {
 	enabled = true,
-	pos = {"TOPRIGHT", UIParent, "TOPLEFT", 280, -20},
-	pos_raid = {"TOPRIGHT", "oUF_sInterfaceRaid", "BOTTOMLEFT", 260, -20}
+	pos = {"TOPRIGHT", Minimap, "BOTTOMRIGHT", 0, -20},
+	pos_raid = {"TOPRIGHT", Minimap, "BOTTOMRIGHT", 0, -20},
 }

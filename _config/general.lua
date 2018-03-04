cda890d04ca243cf2275533750f7fce0be2a1bd6
Sean Baildon <sean@baildon.co>
2018-03-04 15:00:07 +0000

    general: fix issue with config pointing to an invalid font file

--- _config/general.lua
+++ _config/general.lua
@@ -9,17 +9,16 @@ C["general"] = {
 	oocAlpha = 0.3,
 
 	displayFont = {
-		typeface = media.."AlegreyaSans-Bold.otf",
+		typeface = media.."AlegreyaSans-Bold.ttf",
 		size = 12,
 		flag = "OUTLINE"
 	},
 
-	bodyFont= {
-		typeface = media.."AlegreyaSans-Medium.otf",
+	bodyFont = {
+		typeface = media.."AlegreyaSans-Medium.ttf",
 		size = 12,
 		flag = "NONE"
-	},
-
+	}
 }
 
 C["gameinfo"] = {

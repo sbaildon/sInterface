93a407ad8c6a27062155e51a3f5a8780a9c730bf
Sean Baildon <sean@baildon.co>
2018-06-10 10:46:53 +0100

    anchor objective frame to progressbars

--- default.config/objectivetracker.lua
+++ default.config/objectivetracker.lua
@@ -4,6 +4,6 @@ local C = ns.C
 
 C["objectivetracker"] = {
 	enabled = true,
-	pos = {"TOPRIGHT", Minimap, "BOTTOMRIGHT", 0, -20},
+	pos = {"TOPRIGHT", "sInterfaceProgressBars", "BOTTOMRIGHT", 0, -10},
 	pos_raid = {"TOPRIGHT", Minimap, "BOTTOMRIGHT", 0, -20},
 }

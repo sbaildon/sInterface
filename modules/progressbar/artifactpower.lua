2081c45026560b55e0916e7a9386ef203a16dde9
Sean Baildon <sean@baildon.co>
2018-08-11 15:02:18 +0100

    progressbars: artifact power isnt a thing

--- modules/progressbar/artifactpower.lua
+++ modules/progressbar/artifactpower.lua
@@ -2,9 +2,7 @@ local _, ns = ...
 local E, C = ns.E, ns.C
 
 if not C.progressBars.enabled then return end;
-
-local returnEarly = true
-if returnEarly then return end
+if (HasArtifactEquipped() and C_ArtifactUI.IsEquippedArtifactDisabled()) then return end
 
 local ProgressBars = ns.sInterfaceProgressBars
 local barName = "artifactPower"
@@ -14,18 +12,70 @@ local function getArtifactName()
 	return name
 end
 
-local function getArtifactCurrent()
-	if (not HasArtifactEquipped() or UnitHasVehicleUI('player')) then return 0 end
-	local _, _, _, _, totalPower, traitsLearned, _, _, _, _, _, _, tier = C_ArtifactUI.GetEquippedArtifactInfo()
-	local _, power = MainMenuBar_GetNumArtifactTraitsPurchasableFromXP(traitsLearned, totalPower, tier)
-	return power
+local function GetNumTraitsLearnable(numTraitsLearned, power, tier)
+	local numPoints = 0;
+	local powerForNextTrait = C_ArtifactUI.GetCostForPointAtRank(numTraitsLearned, tier)
+	while power >= powerForNextTrait and powerForNextTrait > 0 do
+		power = power - powerForNextTrait
+
+		numTraitsLearned = numTraitsLearned + 1
+		numPoints = numPoints + 1
+
+		powerForNextTrait = C_ArtifactUI.GetCostForPointAtRank(numTraitsLearned, tier)
+	end
+	return numPoints, power, powerForNextTrait
 end
 
--- local function getArtifactMax()
--- 	if (not HasArtifactEquipped() or UnitHasVehicleUI('player')) then return 0 end
--- 	local _, _, _, _, totalPower = C_ArtifactUI.GetEquippedArtifactInfo()
--- 	return totalPower
--- end
+local itemID, altItemID, name, icon, artifactTotalXP, pointsSpent, quality, artifactAppearanceID, appearanceModID, itemAppearanceID, altItemAppearanceID, altOnTop, artifactTier = C_ArtifactUI.GetEquippedArtifactInfo();
+print("itemID", itemID)
+print("altItemID", altItemID)
+print("name", name)
+print("icon", icon)
+print("artifactTotalXP", artifactTotalXP)
+print("pointsSpent", pointsSpent)
+print("quality", quality)
+print("artifactAppearanceID", artifactAppearanceID)
+print("appearanceModID", appearanceModID)
+print("itemAppearanceID", itemAppearanceID)
+print("altItemAppearanceID", altItemAppearanceID)
+print("altOnTop", altOnTop)
+print("artifactTier", artifactTier)
+
+function ArtifactBarGetNumArtifactTraitsPurchasableFromXP(pointsSpent, artifactXP, artifactTier)
+	local numPoints = 0;
+	local xpForNextPoint = C_ArtifactUI.GetCostForPointAtRank(pointsSpent, artifactTier);
+	while artifactXP >= xpForNextPoint and xpForNextPoint > 0 do
+		artifactXP = artifactXP - xpForNextPoint;
+
+		pointsSpent = pointsSpent + 1;
+		numPoints = numPoints + 1;
+
+		xpForNextPoint = C_ArtifactUI.GetCostForPointAtRank(pointsSpent, artifactTier);
+	end
+	return numPoints, artifactXP, xpForNextPoint;
+end
+
+local numPointsAvailableToSpend, xp, xpForNextPoint = ArtifactBarGetNumArtifactTraitsPurchasableFromXP(artifactPointsSpent, artifactTotalXP, artifactTier);
+
+local function getArtifactCurrent()
+	if (not UnitHasVehicleUI('player')) then
+		local azeriteItemLocation = C_AzeriteItem and C_AzeriteItem.FindActiveAzeriteItem()
+		if (azeriteItemLocation) then
+			local power = C_AzeriteItem.GetAzeriteItemXPInfo(azeriteItemLocation)
+			return power
+		elseif (HasArtifactEquipped()) then
+			local _, _, _, _, unspentPower, numTraitsLearned, _, _, _, _, _, _, tier = C_ArtifactUI.GetEquippedArtifactInfo()
+			local _, power = GetNumTraitsLearnable(numTraitsLearned, unspentPower, tier)
+			return power
+		end
+	end
+end
+
+local function getArtifactMax()
+	if (not HasArtifactEquipped() or UnitHasVehicleUI('player')) then return 0 end
+	local _, _, _, _, totalPower = C_ArtifactUI.GetEquippedArtifactInfo()
+	return totalPower
+end
 
 local function getArtifactUntilNext()
 	if (not HasArtifactEquipped() or UnitHasVehicleUI('player')) then return 0 end
@@ -68,10 +118,10 @@ end
 -- end
 
 local function artifactXpUpdate(self)
-	local max = getArtifactNextTraitCost()
+	-- local max = getArtifactNextTraitCost()
 	local cur = getArtifactCurrent()
 
-	self:SetMinMaxValues(0, max)
+	self:SetMinMaxValues(0, 1)
 	self:SetValue(cur)
 end
 

bdf901961c3cc236841839710e8b2fb443276ff8
Sean Baildon <sean@baildon.co>
2018-08-27 19:26:14 +0100

    config: update example

--- config.user/_config.user.lua
+++ config.user/_config.user.lua
@@ -8,12 +8,14 @@ ns.U_C = U_C
 -- U_C["actionbars"] = {
 -- 	bar1 = {
 -- 		visibility = "show",
+-- 		position = { "TOP", oUF_sInterfacePlayer, "BOTTOM", -50, 0 },
 -- 		two_rows = false
 -- 	}
 -- }
 
 -- U_C["uf"] = {
 -- 	hidePlayerFrameOoc = false,
+-- 	emulatePersonalResourceDisplay = false
 -- }
 
 -- U_C["minimap"] = {

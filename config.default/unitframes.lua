f2f9f0a193538ea45d55f3a700ee94133d48129e
Sean Baildon <sean@baildon.co>
2020-11-20 21:30:20 +0000

    config: merge unitframes config

--- config.default/unitframes.lua
+++ config.default/unitframes.lua
@@ -10,100 +10,100 @@ C["uf"] = {
 	emulatePersonalResourceDisplay = true,
 
 	-- Spacing between icons like combo points, arcane orbs, soul shards, etc.
-	classIconSpacing = 4
-}
+	classIconSpacing = 4,
 
-C["uf"].size = {
-	primary = {
-		width = 155,
-		health = 12,
-		power = 3,
+	size = {
+		primary = {
+			width = 155,
+			health = 12,
+			power = 3,
+		},
+		secondary = {
+			width = 135,
+			health = 12,
+			power = 3,
+		},
+		tertiary = {
+			width = 56,
+			health = 7,
+		},
+		raid = {
+			width = 60,
+			health = 12
+		}
 	},
-	secondary = {
-		width = 135,
-		health = 12,
-		power = 3,
+
+	positions = {
+		Player = { "CENTER", UIParent, "CENTER", 0, -175}, -- Unused if emulatePersonalResourceDisplay is set
+		Target = { "TOPLEFT", "oUF_sInterfacePlayer", "TOPRIGHT", 55, 130 },
+		Targettarget = { "TOPLEFT", "oUF_sInterfaceTarget", "TOPRIGHT", 7, 0 },
+		Focus = { "TOPLEFT", "oUF_sInterfacePlayer", "TOPRIGHT", 56, -100 },
+		Focustarget = { "TOPLEFT", "oUF_sInterfaceFocus", "TOPRIGHT", 7, 0 },
+		Pet = { "TOPLEFT", "oUF_sInterfacePlayer", "TOPRIGHT", 5, 0 },
+		Boss = { "BOTTOMLEFT", "oUF_sInterfaceTarget", "TOPRIGHT", 300, 250 },
+		Tank = { "BOTTOMRIGHT", "oUF_sInterfacePlayer", "TOPLEFT", -350, 150 },
+		Raid = { "TOPLEFT", UIParent, "TOPLEFT", 20, -20 },
+		Party = { "BOTTOMRIGHT", "oUF_sInterfacePlayer", "TOPLEFT", -350, 150 },
+		Arena = { "BOTTOMLEFT", "oUF_sInterfaceTarget", "TOPRIGHT", 225, 150 },
 	},
-	tertiary = {
-		width = 56,
-		health = 7,
+
+	aura = {
+		player = {
+			enable = true,
+			mode = "buff",
+			size = 24
+		},
+		target = {
+			enable = true,
+			mode = "aura",
+			size = 18
+		},
+		party = {
+			enable = false,
+			mode = "aura",
+			size = 18
+		},
+		focus = {
+			enable = true,
+			mode = "aura",
+			size = 18
+		},
+		tank = {
+			enable = true,
+			mode = "debuff",
+			size = 30
+		},
+		boss = {
+			enable = false,
+			mode = "debuff",
+			size = 24
+		}
 	},
-	raid = {
-		width = 60,
-		health = 12
+
+	buffs = {
+		-- Unholy DK
+		["Sudden Doom"] = true,
+		["Dark Succor"] = true,
+
+		-- Arms Warrior
+		["Victorious"] = true,
+		["Sharpen Blade"] = true,
+		["Crushing Assault"] = true,
+
+		-- Fury Warrior
+		["Enraged Regeneration"] = true,
+		["Whirlwind"] = true,
+		["Barbarian"] = true,
+		["Battle Trance"] = true,
+
+		-- Arcane Mage
+		["Clearcasting"] = true,
+		["Rule of Threes"] = true,
+		["Displacement Beacon"] = true,
+		["Presence of Mind"] = true,
+
+		-- Fire Mage
+		["Heating Up"] = true,
+		["Hot Streak!"] = true
 	}
-}
-
-C["uf"].positions = {
-	Player = { "CENTER", UIParent, "CENTER", 0, -175}, -- Unused if emulatePersonalResourceDisplay is set
-	Target = { "TOPLEFT", "oUF_sInterfacePlayer", "TOPRIGHT", 55, 130 },
-	Targettarget = { "TOPLEFT", "oUF_sInterfaceTarget", "TOPRIGHT", 7, 0 },
-	Focus = { "TOPLEFT", "oUF_sInterfacePlayer", "TOPRIGHT", 56, -100 },
-	Focustarget = { "TOPLEFT", "oUF_sInterfaceFocus", "TOPRIGHT", 7, 0 },
-	Pet = { "TOPLEFT", "oUF_sInterfacePlayer", "TOPRIGHT", 5, 0 },
-	Boss = { "BOTTOMLEFT", "oUF_sInterfaceTarget", "TOPRIGHT", 300, 250 },
-	Tank = { "BOTTOMRIGHT", "oUF_sInterfacePlayer", "TOPLEFT", -350, 150 },
-	Raid = { "TOPLEFT", UIParent, "TOPLEFT", 20, -20 },
-	Party = { "BOTTOMRIGHT", "oUF_sInterfacePlayer", "TOPLEFT", -350, 150 },
-	Arena = { "BOTTOMLEFT", "oUF_sInterfaceTarget", "TOPRIGHT", 225, 150 },
-}
-
-C["uf"].aura = {
-	player = {
-		enable = true,
-		mode = "buff",
-		size = 24
-	},
-	target = {
-		enable = true,
-		mode = "aura",
-		size = 18
-	},
-	party = {
-		enable = false,
-		mode = "aura",
-		size = 18
-	},
-	focus = {
-		enable = true,
-		mode = "aura",
-		size = 18
-	},
-	tank = {
-		enable = true,
-		mode = "debuff",
-		size = 30
-	},
-	boss = {
-		enable = false,
-		mode = "debuff",
-		size = 24
-	}
-}
-
-C["uf"].buffs = {
-	-- Unholy DK
-	["Sudden Doom"] = true,
-	["Dark Succor"] = true,
-
-	-- Arms Warrior
-	["Victorious"] = true,
-	["Sharpen Blade"] = true,
-	["Crushing Assault"] = true,
-
-	-- Fury Warrior
-	["Enraged Regeneration"] = true,
-	["Whirlwind"] = true,
-	["Barbarian"] = true,
-	["Battle Trance"] = true,
-
-	-- Arcane Mage
-	["Clearcasting"] = true,
-	["Rule of Threes"] = true,
-	["Displacement Beacon"] = true,
-	["Presence of Mind"] = true,
-
-	-- Fire Mage
-	["Heating Up"] = true,
-	["Hot Streak!"] = true
 }
